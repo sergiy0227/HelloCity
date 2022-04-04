2e828eed8970d986651e2cf69c21b6149c3e5903
Sergiy_Marfenko <marfenko.sergiy@ukr.net>
2022-04-04 16:06:54 +0300

    added 1 line

--- src/main/java/HelloCity.java
+++ src/main/java/HelloCity.java
@@ -3,5 +3,6 @@ package main.java;
 public class HelloCity {
     public static void main(String[] args) {
         System.out.println("Hello my favorite City!");
+        System.out.println("Hello my street!");
     }
 }
